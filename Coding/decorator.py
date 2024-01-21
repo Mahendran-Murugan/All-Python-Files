32c77d86694ef541218efb2359212317912755e7
mahi000007 <mahendran1482004@gmail.com>
2024-01-21 21:50:29 +0530

    feat: decorator add

---
+++ Coding/decorator.py
@@ -0,0 +1,36 @@
+# Normal Way
+
+'''
+def add(a, b):
+    return a + b
+
+
+# Funct Keyword is used to pass a Function as Parameter to another Function
+def fun(funct, a, b):
+    return funct(a, b)
+
+
+# here add function is passed as parameter to fun
+print(fun(add, 5, 2))
+
+'''
+
+
+# Decorator Way
+
+
+# Funct Keyword is used to pass a Function as Parameter to another Function
+def fun(funct):
+    def wrapper(a, b):
+        print(funct(a, b))
+
+    return wrapper
+
+
+@fun
+def add(a, b):
+    return a + b
+
+
+# here add function is passed as parameter to fun
+add(5, 2)
